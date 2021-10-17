e7c5d6514062ccc704ce163f4bfa9b0ab8a0fc89
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-17 17:35:00 -0500

    Agregada prelim Comboboxes... agreagr for loop

--- src/components/Censar.vue
+++ src/components/Censar.vue
@@ -4,15 +4,33 @@
     <h2>Agregar Persona</h2>
 
     <form v-on:submit.prevent="metAgregarPersona">
+      
+      <select name="Nombre Combobox Tipo de Doc" id="inpTipoDoc" v-model="persona.tipo_doc">
+        <option value="">Tipo de Documento</option>
+        <option value="CC">Cédula de Ciudadanía</option>
+        <option value="TI">Tarjeta de Identidad</option>
+        <option value="Otro">Otro</option>
+      </select>
+      <br />
+
       <!--label for="inpNDoc">Nombre:</label-->
       <input type="number" placeholder="Número de Documento" id="inpNDoc" v-model="persona.doc_id"
       />
       <br />
+
       <input type="text" placeholder="Nombre Completo" v-model="persona.nombre"/>
       <br />
+
       <input type="date" placeholder="Fecha de Nacimiento" v-model="persona.fechadenacimiento"/>
       <br />
-      <button type="submit">Iniciar Sesion</button>
+
+      <select id="inpOcupacion" v-model="persona.id_ocupacion">
+        <option value="">Ocupación</option>
+      </select>
+      <br />
+      
+      <button type="submit">Agregar</button>
+      <br />
     </form>
   </div>
   <div>Caja en Censar, luego de la caja de formulario <br /></div>
@@ -31,6 +49,7 @@ export default {
 
     return { 
       // Estos valores aparecerán en el formularion en cuanto se cargue la página
+      // Define el v-modelo?
       persona: { 
         tipo_doc: "",
         doc_id: null,
@@ -51,7 +70,9 @@ export default {
 
   methods: {
     metAgregarPersona: function () {
-      alert("Se intento registrar a una persona.");
+      alert("Se intento registrar una persona con los siguientes datos:" + 
+      Object.keys(this.persona) + 
+      Object.values(this.persona));
     },
   },
 };
