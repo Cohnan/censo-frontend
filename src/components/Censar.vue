84d896504f9c2df53432c269d7181096f3ef6cfb
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-17 16:59:07 -0500

    Agregado metodo placeholder para crear persona

--- src/components/Censar.vue
+++ src/components/Censar.vue
@@ -3,8 +3,11 @@
   <div class="cajaFormulario">
     <h2>Agregar Persona</h2>
 
-    <form>
-      <input type="number" placeholder="Numero de Documento" />
+    <form v-on:submit.prevent="metAgregarPersona">
+      <input
+        type="number"
+        placeholder="Número de Documento"
+      />
       <br />
       <input type="text" placeholder="Nombre Completo" />
       <br />
@@ -18,8 +21,39 @@
 
 <!-- Parte JavaScript de mi componente -->
 <script>
-//import axios from "axios";
-export default {};
+//import axios from "axios"; // Para procesar HTTP requests
+
+export default {
+  //Nombre del componente?
+  name: "Censo",
+
+  // Valores iniciales de variables
+  data: function () {
+    return {
+      persona: {
+        tipo_doc: "",
+        doc_id: null,
+        nombre: "",
+        fechadenacimiento: "1900-01-01",
+        departamento: "",
+        id_ocupacion: null,
+        id_etnia: null,
+        id_resguardo: null,
+      },
+    };
+  },
+
+  // Funcion a ejecutar al cargar la pagina
+  created: async function () {
+    
+  },
+
+  methods: {
+    metAgregarPersona: function () {
+      alert("Se intento registrar a una persona.");
+    },
+  },
+};
 </script>
 
 
