52d953d39ce236b14d8d036c1f421e87c444ee92
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-17 15:08:08 -0500

    Formulario sin javscript, parcial

--- src/components/Censar.vue
+++ src/components/Censar.vue
@@ -1,29 +1,28 @@
 <!-- Parte HTML de mi componente -->
 <template>
-    <div class = "cajaFormulario">
-        <h2> Agregar Persona </h2>
+  <div class="cajaFormulario">
+    <h2>Agregar Persona</h2>
 
-        <!--form>
-            <input type="number" v-model="persona.doc_id" placeholder="Numero de Documento"/> <br/>
-            <input type="text" v-model="persona.nombre" placeholder="Nombre Completo"/> <br/>
-            <input type="date" v-model="persona.fechadenacimiento" placeholder="Fecha de Nacimiento"/> <br/>
-            <button type="submit">Iniciar Sesion</button>
-        </form-->
-    </div> 
-
-    Hola censo
-    
+    <form>
+      <input type="number" placeholder="Numero de Documento" />
+      <br />
+      <input type="text" placeholder="Nombre Completo" />
+      <br />
+      <input type="date" placeholder="Fecha de Nacimiento" />
+      <br />
+      <button type="submit">Iniciar Sesion</button>
+    </form>
+  </div>
+  <div>Caja en Censar, luego de la caja de formulario <br /></div>
 </template>
 
 <!-- Parte JavaScript de mi componente -->
 <script>
-export default {
-
-};
+//import axios from "axios";
+export default {};
 </script>
 
 
 <!-- Parte CSS de mi componente -->
 <style>
-
 </style>
