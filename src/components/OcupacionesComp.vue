e067cbb596bc0f08c54a699395c5d009820bb806
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-18 22:03:52 -0500

    Agregar Ocupacion Funciona

--- src/components/OcupacionesComp.vue
+++ src/components/OcupacionesComp.vue
@@ -7,7 +7,11 @@
         <th>Nombre</th>
         <th>Descripcion</th>
       </tr>
-      <tr v-for="ocupacion in ocupaciones" :key="ocupacion.id_ocupacion" v-on:click="metActualizarCampos(ocupacion)">
+      <tr
+        v-for="ocupacion in ocupaciones"
+        :key="ocupacion.id_ocupacion"
+        v-on:click="metActualizarCampos(ocupacion)"
+      >
         <td>{{ ocupacion.id_ocupacion }}</td>
         <td>{{ ocupacion.nombre }}</td>
         <td>{{ ocupacion.descripcion }}</td>
@@ -15,40 +19,71 @@
     </table>
 
     <div class="InputsOcupacion">
-        <input type="text" placeholder="id" v-model="ocupacion.id"/>
-        <input type="text" placeholder="Nombre" v-model="ocupacion.nombre"/>
-        <input type="text" placeholder="Descripcion" v-model="ocupacion.descripcion"/>
+      <input type="text" placeholder="id" v-model="ocupacion.id_ocupacion" />
+      <input type="text" placeholder="Nombre" v-model="ocupacion.nombre" />
+      <input
+        type="text"
+        placeholder="Descripcion"
+        v-model="ocupacion.descripcion"
+      />
     </div>
 
     <div class="CRUD">
-        <button> Agregar </button>
-        <button> Actualizar </button>
-        <button> Eliminar </button>
+      <button v-on:click="metAgregarOcupacion">Agregar</button>
+      <button v-on:click="metActualizarOcupacion">Actualizar</button>
+      <button v-on:click="metEliminarOcupacion">Eliminar</button>
     </div>
-
   </div>
 </template>
 
 <script>
+import axios from "axios"; // Para procesar HTTP requests
+
 export default {
-    name: "OcupacionesComp",
+  name: "OcupacionesComp",
 
-    props: ['ocupaciones'],
+  props: ["ocupaciones"],
 
-    data: function () {
-        return {
-            ocupacion: {
-                id: "",
-                nombre: "",
-                descripcion: ""
-            }
-        };
+  data: function () {
+    return {
+      ocupacion: {
+        id_ocupacion: "",
+        nombre: "",
+        descripcion: "",
+      },
+
+      //ocupaciones: [] // Este ocupaciones se creo luego del props, y con el props funcionaba hasta agregar persona
+    };
+  },
+
+  methods: {
+    metActualizarCampos: function (ocup) {
+      this.ocupacion = ocup;
     },
 
-    methods: {
-        metActualizarCampos: function(ocup) {
-            this.ocupacion = ocup;
-        }
-    }
-}
+    metAgregarOcupacion: async function () {
+      alert(
+        "Se intentara registrar una ocupacion con los siguientes datos:" +
+          Object.entries(this.ocupacion)
+      );
+      axios
+        .post("http://127.0.0.1:8000/ocupaciones/agregar/", this.ocupacion)
+        .then((respuesta) => {
+          alert("Ocupacion agregada exitosamente!: " + respuesta.data);
+        })
+        .catch((error) => {
+          alert("Error: " + error);
+        });
+
+      // Actualizar tabla
+
+      // Borrar campos
+      this.ocupacion = {
+        id_ocupacion: "",
+        nombre: "",
+        descripcion: "",
+      };
+    },
+  },
+};
 </script>
