6f41708c90c266c378adf7ca835a6f24166d0903
sebastian.cohnan@gmail.com <sebastian.cohnan@gmail.com>
2021-10-28 03:49:10 -0500

    Mejorado mensaje Registro Ocupacion

--- src/components/OcupacionesComp.vue
+++ src/components/OcupacionesComp.vue
@@ -41,8 +41,8 @@
 
     <div class="BotonesCrudTablaAd">
       <button v-on:click="metAgregarOcupacion">Agregar</button>
-      <button v-on:click="metActualizarOcupacion" v-if="isAuth">Actualizar</button>
-      <button v-on:click="metEliminarOcupacion" v-if="isAuth">Eliminar</button>
+      <button v-on:click="metActualizarOcupacion" v-if="is_auth" :key="is_auth">Actualizar</button>
+      <button v-on:click="metEliminarOcupacion" v-if="is_auth" :key="is_auth">Eliminar</button>
     </div>
   </div>
 </template>
@@ -72,28 +72,31 @@ export default {
         descripcion: "",
       },
 
-      isAuth: false,
+      is_auth: localStorage.getItem("is_auth"),
     };
   },
 
-  created: function() {
-    this.metRectificarAutenticacion();
+    created: function() {
+      this.metRectificarAutenticacion();
   },
 
   methods: {
     metRectificarAutenticacion: async function () {
       if (localStorage.getItem("token_access") === null || localStorage.getItem("token_refresh") === null) {
-        isAuth = false;
+        localStorage.setItem("is_auth", false);
+        //this.$emit('logOutSuave');
         return;
 			}
 
       return axios.post(appData["direccionBack"] + "refresh/", {refresh: localStorage.getItem("token_refresh")}, {headers: {}})
 				.then((respuesta) => {
 					localStorage.setItem("token_access", respuesta.data.access);	
-          isAuth = true;	
+          localStorage.setItem("is_auth", true);
+          alert("Se rectifico la autenticacion en OcupacionesComp");
 				})
 				.catch(() => {
-					isAuth = false;
+					localStorage.setItem("is_auth", false);
+          //this.$emit('logOutSuave');
 				});
     },
 
@@ -113,9 +116,9 @@ export default {
         )
         .then((respuesta) => {
           alert(
-            "Ocupacion agregada exitosamente!: " +
-              //Object.entries(respuesta.data.registro)
-              JSON.stringify(respuesta.data.registro, null, 2)
+            "Ocupacion agregada exitosamente!: "  + "\n\n" +
+              "Ocupacion: " + respuesta.data.registro.nombre + "\n" +
+              "Descripcion: " + respuesta.data.registro.descripcion
           );
 
           // Borrar campos
@@ -140,7 +143,38 @@ export default {
       );*/
       axios
         .put(
-          "http://127.0.0.1:8000/ocupaciones/",
+          "http://127.0.0.1:8000/ocupaciones/" + this.ocupacionPrelim["id_ocupacion"] + "/",
+          this.ocupacionPrelim
+        ) 
+        .then((respuesta) => {
+          alert(
+            "Ocupacion actualizada exitosamente!: \n\n" +
+              "id: " + respuesta.registro.id_ocupacion + "\n" +
+              "Ocupacion: " + this.ocupacionPrelim.ocupacion + " -> " + respuesta.data.registro.ocupacion + "\n" +
+              "Descripcion: " + this.ocupacionPrelim.descripcion + " -> " + respuesta.data.registro.descripcion
+              //JSON.stringify(respuesta.data.registro, null, 2)
+          );
+
+          // Borrar campos
+          this.ocupacionPrelim = {
+            id_ocupacion: "",
+            nombre: "",
+            descripcion: "",
+          };
+          
+          // Actualizar Variable y Lista de ocupaciones en todos los componentes
+          this.metActualizarListaOcupaciones();
+        })
+        .catch((error) => {
+          alert("Error agregando ocupacion: " + error);
+        });
+    },
+
+    metBorrarOcupacion: function () {
+      
+      axios
+        .delete(
+          appData["direccionBack"] + "ocupaciones/",
           this.ocupacionPrelim
         )
         .then((respuesta) => {
